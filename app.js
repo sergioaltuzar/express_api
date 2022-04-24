de5e1570244e432c7fa1fcd60c8d571bae4df7e0
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 13:40:10 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// Usando objeto express
+const express = require('express');
+// App de Express
+const app = express();
+app.use(express.json()); // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000;
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`);
+});
+
+//http methods
+
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id:1, name: "Sergio1"}
+    const explorer2 = {id:2, name: "Sergio2"}
+    const explorer3 = {id:3, name: "Sergio3"}
+    const explorer4 = {id:1, name: "Sergio4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).JSON(explorers)
+});
