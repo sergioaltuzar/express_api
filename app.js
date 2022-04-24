dc5df1646a0c298a568d68557e4cf86a9254fdbd
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 14:13:52 -0500

    codigo put

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.post('/v1/explorers', (req, res) => {
     res.status(201).json({message: "Created"});
 });
 
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`);
+    console.log(`Update explorer with id ${req.params.id}`);
+    const requestBody = req.body //Paranetros de un cliente des
+    res.status(200).json({message: "Updated!"})
+})
+
 app.delete('/v1/explorers/:id', (req, res) => {
     console.log(`Api Explorers DELETE request ${new Date ()}`);
     console.log(`Delete explorer with id ${req.params.id}`);
