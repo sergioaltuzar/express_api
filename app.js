3978e075b8f3082e0b1d5a50e86bf4fb6e69d3e5
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 13:48:57 -0500

    agregando codigo2

--- app.js
+++ app.js
@@ -22,3 +22,10 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 });
+
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id:1, name:"Sergio"}
+    res.status(200).json(explorers)
+});
